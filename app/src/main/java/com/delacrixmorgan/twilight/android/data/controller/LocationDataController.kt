ea049c655fac4f6ca71480e5560c009fb7e9ff92
Delacrix Morgan <delacrixmorgan@pm.me>
2020-04-19 13:24:02 +0800

    Add Room support for Location

---
+++ app/src/main/java/com/delacrixmorgan/twilight/android/data/controller/LocationDataController.kt
@@ -0,0 +1,16 @@
+package com.delacrixmorgan.twilight.android.data.controller
+
+import com.delacrixmorgan.twilight.android.data.model.Location
+
+object LocationDataController {
+    var locations = mutableListOf<Location>()
+
+    fun getLocationById(uuid: String): Location? {
+        return locations.firstOrNull { it.uuid == uuid }
+    }
+
+    fun getLocation(): List<Location> {
+        var filteredList = locations
+        return filteredList
+    }
+}
---
+++ app/src/main/java/com/delacrixmorgan/twilight/android/data/dao/LocationDataDao.kt
@@ -0,0 +1,19 @@
+package com.delacrixmorgan.twilight.android.data.dao
+
+import androidx.room.*
+import com.delacrixmorgan.twilight.android.data.model.Location
+
+@Dao
+interface LocationDataDao {
+    @Query("SELECT * from Location")
+    suspend fun getLocations(): List<Location>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertLocation(location: Location)
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun updateLocation(location: Location)
+
+    @Query("DELETE from Location")
+    suspend fun deleteLocations()
+}
